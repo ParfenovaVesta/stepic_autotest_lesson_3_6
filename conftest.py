afc57c7ffa336bcfa6dd98a0488b1f3d22717db3
ParfenovaVesta <98935391+ParfenovaVesta@users.noreply.github.com>
2022-02-08 12:09:34 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        action='store',
+        default='Chrome',  # или default=None,
+        help='Choose browser: Chrome, Firefox'
+    )
+    parser.addoption(
+        '--language',
+        action='store',
+        default='ru, en',  # или default=None,
+        help='Language'
+    )
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name').capitalize()
+    language = request.config.getoption('language')
+    print(f'\nStart browser {browser_name} for test..')
+    if browser_name == 'Firefox':
+        options = webdriver.FirefoxProfile()
+        options.set_preference("intl.accept_languages", language)
+        driver = webdriver.Firefox(firefox_profile=options)
+    else:
+        if browser_name != 'Chrome':
+            print(f'\nDriver {browser_name} not found!')
+            print('\nStart default browser Chrome for test..')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        driver = webdriver.Chrome(options=options)
+    # driver.implicitly_wait(5)
+
+    yield driver
+
+    print('\nQuit browser..')
+    driver.quit()
---
+++ test_items.py
@@ -0,0 +1,16 @@
+import pytest
+import time
+
+links = [
+    'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+]
+
+
+@pytest.mark.parametrize('link', links)
+def test_find_add_to_basket_button(browser, link):
+    browser.get(link)
+    curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    time.sleep(30)
+    find_buttons = browser.find_elements_by_css_selector('#add_to_basket_form .btn-add-to-basket')
+    assert len(find_buttons) > 0, 'Отсуствует кнопка добавления в корзину'
+
